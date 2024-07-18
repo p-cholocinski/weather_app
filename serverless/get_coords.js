8f2bb7ec9a31137e81b4bd09f52970959d71a8d3
p-cholocinski <piotr.cholocinski@gmail.com>
2024-07-18 21:16:43 +0200

    add module to type module to package.json, dlete import fetch from dataFunctions, replace require to import in serverless functions

--- serverless/get_coords.js
+++ serverless/get_coords.js
@@ -1,8 +1,8 @@
-const fetch = require("node-fetch");
+import fetch from "node-fetch";
 
 const { WEATHER_API_KEY } = process.env;
 
-export async function handler(event, context) {
+exports.handler = async (event, context) => {
     const params = JSON.parse(event.body);
     const { text, units } = params;
     const regex = /^\d+$/g;
