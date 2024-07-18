2d62a59d706a214f3fc29e050555dd6dab03f46f
p-cholocinski <piotr.cholocinski@gmail.com>
2024-07-18 22:22:11 +0200

    change text to entryText in get_coords function in serverless

--- serverless/get_coords.js
+++ serverless/get_coords.js
@@ -4,7 +4,7 @@ const { WEATHER_API_KEY } = process.env;
 
 export async function handler(event, context) {
     const params = JSON.parse(event.body);
-    const { text, units } = params;
+    const { entryText, units } = params;
     const regex = /^\d+$/g;
     const flag = regex.test(text) ? "zip" : "q";
     const url = `https://api.openweathermap.org/data/2.5/weather?${flag}=${entryText}&units=${units}&appid=${WEATHER_API_KEY}`;
